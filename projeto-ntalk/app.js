2c9005f8d1b61787e02971f327e8d0333908f3b4
tonhus <antonio_abrantes@hotmail.com.br>
2019-11-01 10:31:23 -0300

    capitulo 05

--- projeto-ntalk/app.js
+++ projeto-ntalk/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const methodOverride = require('method-override');
+//const error = require('./middlewares/error');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession({ resave: true, saveUninitialized: true, secret: 'ntalk' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 consign({})
@@ -15,5 +25,5 @@ consign({})
 ;
 
 app.listen(3000, () => {
-  console.log('Ntalk no ar.');
+  console.log('Ntalk rodando na porta 3000.');
 });
--- projeto-ntalk/controllers/home.js
+++ projeto-ntalk/controllers/home.js
@@ -1,7 +1,27 @@
-module.exports = function (app) {
-    var HomeController = {
-        index: function (req, res) {
+module.exports = (app) => {
+    const HomeController = {
+        index(req, res) {
             res.render('home/index');
+        },
+        login(req, res) {
+            const {
+                usuario
+            } = req.body;
+            const {
+                email,
+                nome
+            } = usuario;
+            if (email && nome) {
+                usuario.contatos = [];
+                req.session.usuario = usuario;
+                res.redirect('/contatos');
+            } else {
+                res.redirect('/');
+            }
+        },
+        logout(req, res) {
+            req.session.destroy();
+            res.redirect('/');
         }
     };
     return HomeController;
